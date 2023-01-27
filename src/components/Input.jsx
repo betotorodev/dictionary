7faaa9fe9e434a2e88f4ea47ce5afa52c12d6962
Beto Toro <betotoro0902@gmail.com>
2023-01-27 09:29:58 -0500

    feat: start the meaning layout

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,26 @@
 export const Input = () => {
   return (
-    <div class='flex justify-between w-full px-[24px] py-[20px] text-xl font-bold bg-[#F4F4F4] rounded-2xl mt-[51px] cursor-pointer' type='text'>
-      <input class='bg-transparent outline-none' placeholder='Search your word' />
-      <img src='/assets/images/icon-search.svg' alt='a search icon to load the info' />
-    </div>
+    <main>
+      {/* input */}
+      <div class='flex justify-between w-full px-[24px] py-[20px] text-xl font-bold bg-[#F4F4F4] rounded-2xl mt-[51px] cursor-pointer' type='text'>
+        <input class='bg-transparent outline-none' placeholder='Search your word' />
+        <img src='/assets/images/icon-search.svg' alt='a search icon to load the info' />
+      </div>
+      {/* word */}
+      <section class='flex justify-between mt-11'>
+        <div>
+          <h1 class='text-[64px] font-bold'>Keyboard</h1>
+          <h3 class='text-[24px] text-[#A445ED]'>/ˈkiːbɔːd/</h3>
+        </div>
+        <img src='/assets/images/icon-play.svg' alt='and icon of play to play an audio' />
+      </section>
+      {/* results */}
+      <section class='mt-[40px]'>
+        <div class='flex gap-4 items-center'>
+          <h2 class='text-[24px] font-bold italic'>noun</h2>
+          <div class='w-full h-[2px] bg-slate-200'/>
+        </div>
+      </section>
+    </main>
   )
 }
