d788352b7c5de6554ab7d001563cf6ab7aa2decd
Beto Toro <betotoro0902@gmail.com>
2023-01-31 15:35:14 -0500

    feat: use the slot pattern to use the loader.astro component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,53 +1,36 @@
-import { useState } from 'preact/hooks'
+import { useWord, DATA_STATE } from '../hooks/useWord'
+import { useInput } from '../hooks/useInput'
 
-const API_URL = 'https://wordsapiv1.p.rapidapi.com/words/'
+export const Input = ({ children }) => {
+  const { data, loading, updateWord } = useWord()
+  const { inputValue, handleInput, clearInput } = useInput()
 
-export const Input = () => {
-  const [word, setWord] = useState('')
-  const [data, setData] = useState({})
-  const handleChange = (event) => {
-    const value = event.target.value
-    setWord(value)
-  }
   const handleKeyDown = (event) => {
-    const isNotEmpty = !!word
+    const isNotEmpty = !!inputValue
     if (event.key === 'Enter' && isNotEmpty) {
-      getWordDefinition()
-      setWord('')
+      updateWord(inputValue)
+      clearInput()
     }
   }
-  const getWordDefinition = () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'f47ec3cdfcmshbbe3ab04c3e297fp135cdfjsn0c40e5353898',
-        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-      }
-    }
 
-    fetch(`${API_URL}${word}`, options)
-      .then(response => response.json())
-      .then(response => {
-        setData(response)
-        console.log(response)
-      })
-      .catch(err => console.error(err))
-  }
   return (
     <main>
       {/* input */}
       <div class='flex justify-between w-full px-[24px] py-[20px] text-xl font-bold bg-[#F4F4F4] rounded-2xl mt-[51px] cursor-pointer' type='text'>
-        <input onKeyDown={handleKeyDown} onInput={handleChange} value={word} class='bg-transparent outline-none' placeholder='Search your word' />
+        <input onKeyDown={handleKeyDown} onInput={handleInput} value={inputValue} class='bg-transparent outline-none' placeholder='Search your word' />
         <img src='/assets/images/icon-search.svg' alt='a search icon to load the info' />
       </div>
       {/* word */}
       {
-        !!data.word && (
+      // eslint-disable-next-line multiline-ternary, no-extra-boolean-cast
+      !!data?.word && loading === DATA_STATE.COMPLETED && (
           <>
             <section class='flex justify-between mt-11'>
               <div>
-                <h1 class='text-[64px] font-bold'>{data.word}</h1>
-                <h3 class='text-[24px] text-[#A445ED]'>/{data.pronunciation.all}/</h3>
+                <h1 class='text-[64px] font-bold'>{data?.word}</h1>
+                <h3 class='text-[24px] text-[#A445ED]'>
+                  /{data?.pronunciation.all}/
+                </h3>
               </div>
               {/* <img src='/assets/images/icon-play.svg' alt='and icon of play to play an audio' /> */}
             </section>
@@ -60,15 +43,16 @@ export const Input = () => {
               <div class='mt-10'>
                 <h3 class='text-[#757575] text-xl mb-6'>Meaning</h3>
                 <ol class='pl-8'>
-                  {
-                    data.results?.map((item, index) => <li key={index} class='py-[13px]'>{item.definition}</li>)
-                  }
+                  {data?.results?.map((item, index) => (
+                    <li key={index} class='py-[13px]'>{item.definition}</li>
+                  ))}
                 </ol>
               </div>
             </section>
           </>
         )
       }
+      {loading === DATA_STATE.LOADING && <>{children}</>}
     </main>
   )
 }
