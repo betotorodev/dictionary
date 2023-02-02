ef856a929cc5f4b1b9a799e4ab5c2c52931e2e08
Beto Toro <betotoro0902@gmail.com>
2023-02-02 09:18:07 -0500

    feat: add some js to implement de dark mode

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -18,9 +18,9 @@ export const Input = ({ children }) => {
   }, [loading])
   
   return (
-    <main>
+    <main id='results'>
       {/* input */}
-      <div class='flex justify-between w-full px-[24px] py-[20px] text-xl font-bold bg-[#F4F4F4] rounded-2xl mt-[51px] cursor-pointer' type='text'>
+      <div class='flex justify-between w-full px-[24px] py-[20px] text-xl font-bold bg-[#F4F4F4] rounded-2xl mt-[51px] cursor-pointer' type='text' id='search'>
         <input onKeyDown={handleKeyDown} onInput={handleInput} value={inputValue} class='bg-transparent outline-none' placeholder='Search your word' />
         <img src='/assets/images/icon-search.svg' alt='a search icon to load the info' />
       </div>
